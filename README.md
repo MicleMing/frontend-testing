4d1b505da0e0dbbad31f8c44dd46a5f6d568178f
MicleMing <fzulanming@gmail.com>
2018-08-12 12:36:52 +0800

    test: unit test for LockScreen

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## unit test
 ## integrate test
 ## e2e test
+
+## Reference resources
+  - [react-testing-example-lockscreen](https://github.com/suchipi/react-testing-example-lockscreen.git)
+  - [The Right Way to Test React Components](https://medium.freecodecamp.org/the-right-way-to-test-react-components-548a4736ab22)
+  - [Testing Your Frontend Code](https://hackernoon.com/testing-your-frontend-code-part-i-introduction-7e307eac4446)
--- test/unit/lockScreen.test.js
+++ test/unit/lockScreen.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import LockScreen from '../../src/components/LockScreen';
+import ClockDisplay from '../../src/components/ClockDisplay';
+import SlideToUnlock from '../../src/components/SlideToUnlock';
+import TopOverlay from '../../src/components/TopOverlay';
 
 describe('lock screen', () => {
   let props;
@@ -14,7 +17,6 @@ describe('lock screen', () => {
     }
     return mountedLockScreen;
   }
-
   beforeEach(() => {
     props = {
       wallpaperPath: undefined,
@@ -24,8 +26,75 @@ describe('lock screen', () => {
     mountedLockScreen = undefined;
   });
 
-  it("always renders a div", () => {
-    const divs = lockScreen().find("div");
+  it('always renders a div', () => {
+    const divs = lockScreen().find('div');
     expect(divs.length).toBeGreaterThan(0);
+  });
+
+  it('The rendered div contains everything else that gets rendered', () => {
+    const divs = lockScreen().find('div');
+    const screen1 = divs.first();
+    const screen2 = lockScreen().children();
+    expect(screen1).toEqual(screen2);
+  });
+
+  it('always renders a `ClockDisplay`', () => {
+    const clockDisplay = lockScreen().find(ClockDisplay);
+    expect(clockDisplay.length).toEqual(1);
+  });
+
+  it('always renders a `SlideToUnlock`', () => {
+    const slideToUnlock = lockScreen().find(SlideToUnlock);
+    expect(slideToUnlock.length).toEqual(1);
+  });
+
+  describe('check `onUnlocked`', () => {
+    it("sets the rendered `SlideToUnlock`'s `onSlide` prop to the same value as `onUnlocked`", () => {
+      props.onUnlocked = jest.fn();
+      const slideToUnlock = lockScreen().find(SlideToUnlock);
+      expect(slideToUnlock.props().onSlide).toEqual(props.onUnlocked);
+    });
+
+    it("sets the rendered `SlideToUnlock`'s `onSlide` prop to undefined", () => {
+      props.onUnlocked = undefined;
+      const slideToUnlock = lockScreen().find(SlideToUnlock);
+      expect(slideToUnlock.props().onSlide).toBeUndefined();
+    })
+  });
+
+  describe('when `wallpaperPath` is passed', () => {
+    beforeEach(() => {
+      props.wallpaperPath = 'test.png';
+    })
+
+    it('applies that wallpaper as a background-image on the wrapping div', () => {
+      const wrappingDiv = lockScreen().find('div').first();
+      expect(wrappingDiv.props().style.backgroundImage).toBe(`url(${props.wallpaperPath})`);
+    })
+  })
+
+  describe('when `userInfoMessage` is passed', () => {
+    beforeEach(() => {
+      props.userInfoMessage = 'This is my favorite phone!';
+    });
+
+    it('renders a `TopOverlay`', () => {
+      expect(lockScreen().find(TopOverlay).length).toBe(1);
+    });
+
+    it('passes `userInfoMessage` to the rendered `TopOverlay` as `children`', () => {
+      const topOverlay = lockScreen().find(TopOverlay);
+      expect(topOverlay.props().children).toBe(props.userInfoMessage);
+    });
+  });
+
+  describe('when `userInfoMessage` is undefined', () => {
+    beforeEach(() => {
+      props.userInfoMessage = undefined;
+    });
+
+    it('does not render a `TopOverlay`', () => {
+      expect(lockScreen().find(TopOverlay).length).toBe(0);
+    });
   });
 })

618d9dee94c6e99c7614d28c9de61402661ba468
MicleMing <fzulanming@gmail.com>
2018-08-14 23:11:59 +0800

    test: update unit test for ClockDisplay

--- test/unit/clockDisplay.test.js
+++ test/unit/clockDisplay.test.js
@@ -26,21 +26,30 @@ describe('clock display', async () => {
     }
   })
 
-  it('call componentDidMount once', () => {
+  it('call componentDidMount once and set timer', () => {
     sinon.spy(ClockDisplay.prototype, 'componentDidMount');
-    const mountedClockDisplay = clockDisplay();
-    expect(ClockDisplay.prototype.componentDidMount.callCount).toEqual(1)
-    // const time1 = mountedClockDisplay.state('now');
-    // await sleep(1000);
-    // const time2 = mountedClockDisplay.state('now')
-    // expect(time1.getTime()).toEqual(time2.getTime());
+    const instance = clockDisplay().instance();
+    expect(ClockDisplay.prototype.componentDidMount.callCount).toEqual(1);
+    expect(instance.updateInterval).toMatchObject({
+      _repeat: 500
+    });
   });
 
-  it('call componentWillUnmount once', () => {
+  it('call componentWillUnmount once and clear timer', () => {
     sinon.spy(ClockDisplay.prototype, 'componentWillUnmount');
-    const clockDisplayEntity = clockDisplay();
-    clockDisplayEntity.unmount();
-    expect(ClockDisplay.prototype.componentWillUnmount.callCount).toEqual(1)
-  })
+    const instance = clockDisplay().instance();
+    mountedClockDisplay.unmount();
+    expect(ClockDisplay.prototype.componentWillUnmount.callCount).toEqual(1);
+    expect(instance.updateInterval).toMatchObject({
+      _repeat: null
+    })
+  });
+
+  it('call setInterval with 500 ms', () => {
+    jest.useFakeTimers();
+    clockDisplay();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+    expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 500);
+  });
 
 })
